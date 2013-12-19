0d503db992e0ce03710c8959b60243a7af49fab4
Jarad DeLorenzo <jaradd@gmail.com>
2013-12-19 09:22:21 -0500

    Fixed #1113 Implemented contact form. Implemented form validation

--- app/scripts/controllers/ContactController.js
+++ app/scripts/controllers/ContactController.js
@@ -2,7 +2,43 @@ SagepointPortal.ContactController = Ember.ObjectController.extend({
 
 	actions: {
 		send: function() {
-			console.log("Sent!");
+			var email = this.get('fromEmail');
+			var body = this.get('body');
+
+			if(!email.match(/.+@.+\..{2,4}/)) {
+				$.notifyBar({html: "Please enter a valid email", cssClass: "error"});
+				return;
+			}
+
+			if(body === null || body.length < 15) {
+				$.notifyBar({html: "Please enter a minimum of 15 characters in your message", cssClass: "error"});
+				return;
+			}
+
+			$.ajax({
+				type: 'POST',
+				url: SagepointPortal.api_base + '/' + SagepointPortal.api_version + '/contact',
+				data: {
+					fromEmail: email,
+					body: body,
+				},
+				success: function(data) {
+					if(data.success === true) {
+						$.notifyBar({html: data.message, cssClass: "success"});
+					} else {
+						$.notifyBar({html: data.errors[0], cssClass: "error"});
+					}
+
+				},
+				error: function(xhr, status, errorThrown) {
+					if(typeof xhr.responseText === 'undefined') {
+						jQuery.notifyBar({html: "An connection error occured", cssClass: 'error'});
+					} else {
+						jQuery.notifyBar({html: JSON.parse(xhr.responseText).errors[0], cssClass: 'error'});
+					}
+				},
+				dataType: 'json'
+			});
 		}
 	}
 
--- app/scripts/routes/contact_route.js
+++ app/scripts/routes/contact_route.js
@@ -2,8 +2,8 @@ SagepointPortal.ContactRoute = Ember.Route.extend({
 	model: function() {
 		return {
 			"fromEmail": null,
-			"subjects": ["I'm having a technical problem", "I can't login", "You need someone to talk to"],
-			"selectedSubject": "I can't login",
+			// "subjects": ["I'm having a technical problem", "I can't login", "You need someone to talk to"],
+			// "selectedSubject": "I can't login",
 			"body": null
 		};
 	}
